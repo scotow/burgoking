af916228775199a49e552f7a7191316687c087f3
Benjamin Lopez <contact@scotow.com>
2019-01-21 15:48:57 +0100

    fix waiting message and error message typo

--- cmd/web/static/js/app.js
+++ cmd/web/static/js/app.js
@@ -3,41 +3,37 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('label').innerText = text;
     }
 
-    function errorOccurred() {
-        const genericError = 'An error has occurred. Feel free to send me an email to the admin of the website.';
+    function sendRequest(method, url, onLoad, onSuccess, onError) {
+        const request = new XMLHttpRequest();
 
         request.onreadystatechange = function() {
             if (request.readyState === XMLHttpRequest.DONE) {
                 if (request.status === 200) {
-                    setLabel('An error has occurred. Feel free to send me an email at \'' + request.responseText + '\' to help me improve this project.');
+                    if (onSuccess) onSuccess(request);
                 } else {
-                    setLabel(genericError);
+                    if (onError) onError(request);
                 }
+            } else if (request.readyState === XMLHttpRequest.OPENED) {
+                if (onLoad) onLoad();
             }
         };
-        request.onerror = function () {
-            setLabel(genericError);
-        };
+        request.onerror = onError;
 
-        request.open('GET', '/contact', true);
+        request.open(method, url, true);
         request.send(null);
     }
 
-    const request = new XMLHttpRequest();
+    function errorOccurred() {
+        sendRequest('GET', '/contact', null, function (request) {
+            setLabel('An error has occurred. Feel free to send me an email at \'' + request.responseText + '\' to help me improve this project.');
+        }, function () {
+            setLabel('An error has occurred. Feel free to send an email to the admin of the website.');
+        });
+    }
 
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                setLabel(request.responseText);
-            } else {
-                errorOccurred()
-            }
-        } else if (request.readyState === XMLHttpRequest.LOADING) {
-            setLabel('Loading your Burger');
-        }
-    };
-    request.onerror = errorOccurred;
-
-    request.open('POST', '/code', true);
-    request.send(null);
+    sendRequest('POST', '/code', function() {
+        setLabel('Loading your Burger');
+    }, function (request) {
+        setLabel(request.responseText);
+    }, errorOccurred);
 });
